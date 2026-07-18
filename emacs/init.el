b02bd5ff377439b2be0e9dec15c2a6119034a2a9
nasr <nsrddyn@gmail.com>
2026-07-18 12:38:13 +0200

    checkpoint

--- emacs/init.el
+++ emacs/init.el
@@ -1,6 +1,5 @@
 (require 'package)
 
-
 (setq package-archives
       '(("gnu"   . "https://elpa.gnu.org/packages/")
         ("melpa" . "https://melpa.org/packages/")))
@@ -16,17 +15,20 @@
 (setq use-package-always-ensure t)
 
 
-;; Startup
 (setq inhibit-startup-screen t
       inhibit-startup-message t
       inhibit-startup-echo-area-message t
       ring-bell-function 'ignore
       use-dialog-box nil)
 
-;; Remove default UI elements
-(menu-bar-mode -1)
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+(menu-bar-mode 1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(column-number-mode -1)
+(show-paren-mode -1)
 
 ;; Cursor
 (setq cursor-type 'hbar)
@@ -40,23 +42,15 @@
 (global-auto-revert-mode 1)
 
 
-;; Native compilation settings (Emacs 28+)
 (setq native-comp-async-report-warnings-errors nil
       native-comp-speed 2)
 
-;; Garbage collection and process I/O optimization
-(setq gc-cons-threshold (* 100 1024 1024)
-      read-process-output-max (* 1024 1024)
-      inhibit-compacting-font-caches t
-      bidi-display-reordering nil
-      frame-resize-pixelwise nil
-      window-resize-pixelwise nil)
 
 
 ;; Font and colors
 (set-face-attribute 'default nil
                     :font "liberation mono"
-                    :height 150 
+                    :height 120 
                     :foreground "#C4A882"
                     :background "#000000")
 
@@ -64,30 +58,28 @@
 
 ;; Face customization 
 (custom-set-faces
-
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(cursor ((t (:background "#F0A030"))))
-
- '(font-lock-builtin-face           ((t (:foreground "#D4A030"))))
- '(font-lock-comment-face           ((t (:foreground "#6B5A45"))))
- '(font-lock-constant-face          ((t (:foreground "#9B7040"))))
- '(font-lock-function-name-face     ((t (:foreground "#C4A882"))))
- '(font-lock-keyword-face           ((t (:foreground "#D48B30"))))
- '(font-lock-preprocessor-face      ((t (:foreground "#C05020"))))
- '(font-lock-string-face            ((t (:foreground "#B86030"))))
- '(font-lock-type-face              ((t (:foreground "#B87040"))))
- '(font-lock-variable-name-face     ((t (:foreground "#C4A882"))))
- '(font-lock-warning-face           ((t (:foreground "#DC4A20"))))
-
- '(hl-line                          ((t (:background "#1F1610"))))
- '(isearch                          ((t (:background "#3D2B1A" :foreground "#C4A882"))))
-
- '(lazy-highlight                   ((t (:background "#3D2B1A"))))
-
- '(line-number                      ((t (:foreground "#5A4A38"))))
- '(line-number-current-line         ((t (:foreground "#D48B30"))))
-
- '(region                           ((t (:background "#3A0A00"))))
- '(mode-line                        ((t (:background "#C4A882" :foreground "#5A4A38" :box nil)))))
+ '(font-lock-builtin-face ((t (:foreground "#D4A030"))))
+ '(font-lock-comment-face ((t (:foreground "#6B5A45"))))
+ '(font-lock-constant-face ((t (:foreground "#9B7040"))))
+ '(font-lock-function-name-face ((t (:foreground "#C4A882"))))
+ '(font-lock-keyword-face ((t (:foreground "#D48B30"))))
+ '(font-lock-preprocessor-face ((t (:foreground "#C05020"))))
+ '(font-lock-string-face ((t (:foreground "#B86030"))))
+ '(font-lock-type-face ((t (:foreground "#B87040"))))
+ '(font-lock-variable-name-face ((t (:foreground "#C4A882"))))
+ '(font-lock-warning-face ((t (:foreground "#DC4A20"))))
+ '(hl-line ((t (:background "#1F1610"))))
+ '(isearch ((t (:background "#3D2B1A" :foreground "#C4A882"))))
+ '(lazy-highlight ((t (:background "#3D2B1A"))))
+ '(line-number ((t (:foreground "#5A4A38"))))
+ '(line-number-current-line ((t (:foreground "#D48B30"))))
+ '(mode-line ((t (:background "#C4A882" :foreground "#5A4A38" :box nil))))
+ '(region ((t (:background "#3A0A00")))))
 
 
 ;; Evil mode 
@@ -116,5 +108,32 @@
 (global-display-line-numbers-mode -1)
 (global-hl-line-mode 1)
 
+
+; multiple cursors
+(use-package multiple-cursors
+  :ensure t)
+
+(global-set-key (kbd "C-q") 'mc/edit-lines)
+(global-set-key (kbd "C->")         'mc/mark-next-like-this)
+(global-set-key (kbd "C-<")         'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<")     'mc/mark-all-like-this)
+(global-set-key (kbd "C-\"")        'mc/skip-to-next-like-this)
+(global-set-key (kbd "C-:")         'mc/skip-to-previous-like-this)
+
+;;; dired
+(require 'dired-x)
+(setq dired-omit-files
+      (concat dired-omit-files "\\|^\\..+$"))
+(setq-default dired-dwim-target t)
+(setq dired-listing-switches "-alh")
+(setq dired-mouse-drag-files t)
+
+
+(require 'ede)
+(global-ede-mode t)
 (custom-set-variables
- '(package-selected-packages nil))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ede-project-directories '("/home/nasr/tb")))
